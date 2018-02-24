9acf7951ab6e4dc245177cd69ed240c2c29a5da9
Akos Szokodi <szokodiakos@gmail.com>
2018-02-25 00:38:39 +0100

    add config

--- leaderboard/leaderboard_printer_slack.go
+++ leaderboard/leaderboard_printer_slack.go
@@ -7,18 +7,21 @@ import (
 )
 
 type slackPrinter struct {
+	webhook string
 }
 
 // Print content via Slack webhook
 func (p *slackPrinter) Print() {
 	reader := bytes.NewReader([]byte(`{ "text": "Hello from Go" }`))
-	_, err := http.Post("https://hooks.slack.com/services/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", "application/json", reader)
+	_, err := http.Post(p.webhook, "application/json", reader)
 	if err != nil {
 		fmt.Println("Err", err)
 	}
 }
 
 // NewSlackPrinter creates a slackPrinter
-func NewSlackPrinter() Printer {
-	return &slackPrinter{}
+func NewSlackPrinter(webhook string) Printer {
+	return &slackPrinter{
+		webhook,
+	}
 }
--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 	"github.com/szokodiakos/r8m8/leaderboard"
 )
 
+func setupConfig() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.SetEnvPrefix("r8m8")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal("Error reading in config.json")
+	}
+}
+
 func main() {
-	lb := leaderboard.NewService(leaderboard.NewSlackPrinter())
+	setupConfig()
+
+	lb := leaderboard.NewService(leaderboard.NewSlackPrinter(viper.GetString("webhook")))
 	lb.GetLeaderboard()
 }
