1db30c8b91e3008a02cae98838fcc3c2b3c0ed96
Akos Szokodi <szokodiakos@gmail.com>
2018-03-27 19:08:44 +0200

    refactor shorter query logs

--- sql/sql_transaction.go
+++ sql/sql_transaction.go
@@ -2,6 +2,8 @@ package sql
 
 import (
 	"database/sql"
+	"fmt"
+	"strings"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/jmoiron/sqlx"
@@ -32,22 +34,38 @@ func (t *transaction) Rollback() error {
 }
 
 func (t *transaction) Exec(query string, args ...interface{}) (sql.Result, error) {
-	logger.Get().Info(query, spew.Sdump(args))
+	logger.Get().Info(formatQueryString(query), spew.Sdump(args))
 	return t.tx.Exec(query, args...)
 }
 
 func (t *transaction) Select(dest interface{}, query string, args ...interface{}) error {
-	logger.Get().Info(query, spew.Sdump(args))
+	logger.Get().Info(formatQueryString(query), spew.Sdump(args))
 	err := t.tx.Select(dest, query, args...)
 	logger.Get().Info(spew.Sdump(dest))
 	return err
 }
 
 func (t *transaction) Get(dest interface{}, query string, args ...interface{}) error {
-	logger.Get().Info(query, spew.Sdump(args))
+	logger.Get().Info(formatQueryString(query), spew.Sdump(args))
 	err := t.tx.Get(dest, query, args...)
 	logger.Get().Info(spew.Sdump(dest))
 	return err
+}
+
+func formatQueryString(query string) string {
+	return shorten(withoutTabs(withoutNewLines(query)))
+}
+
+func withoutNewLines(input string) string {
+	return strings.Replace(input, "\n", "", -1)
+}
+
+func withoutTabs(input string) string {
+	return strings.Replace(input, "\t", "", -1)
+}
+
+func shorten(input string) string {
+	return fmt.Sprintf("%v...", input[:50])
 }
 
 // NewSQLTransaction factory
